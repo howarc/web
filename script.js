dd6d9bbb4170d6b550fdf3bcb2bf0019598e2b8b
howarc <howardchen187@gmail.com>
2024-05-28 20:52:54 -0400

    fixed more bugs

--- script.js
+++ script.js
@@ -28,23 +28,9 @@ function adjustOverflowBasedOnZoom() {
     } else {
         aboutSection.style.overflow = 'hidden';
         projectsSection.style.overflow ='hidden';
-        contactSection.Style.overflow = 'hidden';
+        contactSection.style.overflow = 'hidden';
     }
 }
 
 window.addEventListener('resize', adjustOverflowBasedOnZoom);
 window.onload = adjustOverflowBasedOnZoom;
-
-document.getElementById("menuButton").addEventListener("click", function() {
-    var items = document.getElementById("menuItems");
-    if (items.style.display === "block") {
-        items.style.display = "none";
-        items.style.opacity = 0;
-    } else {
-        items.style.display = "block";
-        setTimeout(function() {
-            items.style.opacity = 1;
-        }, 10); // Enable transition
-    }
-});
-
